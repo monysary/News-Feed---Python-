874564a8e62d40bb24738bb0881cfe8e779e4655
monysary <sary.mony@gmail.com>
2023-04-05 12:59:43 -0700

    added password encryption in user models

--- app/models/User.py
+++ app/models/User.py
@@ -1,6 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
 
+salt = bcrypt.gensalt()
 
 class User(Base):
     __tablename__ = 'users'
@@ -19,4 +22,4 @@ class User(Base):
     def validate_password(self, key, password):
         # ensures password length is more than 4 characters
         assert len(password) > 4
-        return password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
