6784014e68222a4b9f20c6eeae3cdf88f208d04e
monysary <sary.mony@gmail.com>
2023-04-05 12:48:31 -0700

    updated user's email and password model with validations

--- app/models/User.py
+++ app/models/User.py
@@ -1,9 +1,22 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
 
+
 class User(Base):
-  __tablename__ = 'users'
-  id = Column(Integer, primary_key=True)
-  username = Column(String(50), nullable=False)
-  email = Column(String(50), nullable=False, unique=True)
-  password = Column(String(100), nullable=False)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        # ensures password length is more than 4 characters
+        assert len(password) > 4
+        return password
